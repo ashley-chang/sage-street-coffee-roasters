65299f2d841387dde6275657e84fc3490c912d60
Ashley <ashleychang96@gmail.com>
2018-11-30 02:00:28 -0800

    Change MONGODB_URI

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(passport.session());
 app.use(csrfProtection);
 
 app.use(express.static(path.join(__dirname, "dist")));
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/shopping').then(() => {
+mongoose.connect(process.env.MONGODB_URI).then(() => {
   console.log("Connected to Database");
 }).catch((err) => {
   console.log("Not connected to Database: ", err);
