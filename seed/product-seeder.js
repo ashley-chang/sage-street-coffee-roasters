3e1e1e8c9b18b8854521b93956122d5b065cb4cf
Ashley <ashleychang96@gmail.com>
2018-11-30 02:07:09 -0800

    Fix typo in  MONGODB_URI in seeder files

--- seed/product-seeder.js
+++ seed/product-seeder.js
@@ -2,7 +2,7 @@
 const Product = require('../models/product');
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGDB_URI);
+mongoose.connect(process.env.MONGODB_URI);
 
 //create a product
 const products = [
